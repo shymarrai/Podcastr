575ccc98193b25cb4c7569d82719f7244655cf2c
Bruno Gomes <icestonebruno@gmail.com>
2021-05-27 15:43:12 -0300

    tentativa com api de prota fixa

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "https://podcastr-n8cuucr9b-shymarrai.vercel.app:3333/"
+  baseURL: "https://server-json-podcastr.herokuapp.com/:31081/"
 })
